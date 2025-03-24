b0c79b04a6e89e8cf857bfd6e06f82bc143968b9
FtBunny <bunnyy129@gmail.com>
2025-03-25 01:40:30 +0530

    `Added Siri wave feature and updated UI components`

---
+++ engine/features.py
@@ -0,0 +1,7 @@
+from playsound import playsound
+
+# Play assistant sound
+def playAssistantSound():
+    music_dir = r"www\\assets\audio\start_sound.mp3"
+    playsound(music_dir)
+    
--- www/main.js
+++ www/main.js
@@ -13,5 +13,33 @@ $(document).ready(function () {
 
     });
 
+    //siriwave
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 800,
+        height: 200,
+        style: "ios9",
+        amplitude: "1",
+        speed: "0.3",
+        autostart: true
+    });
+
+    //siri message
+    $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "fadeInUp",
+            sync: true,
+        },
+        out: {
+            effect: "fadeOutUp",
+            sync: true,
+        },
+
+    });
+
+
 });
  
+
